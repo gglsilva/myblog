9fdcf4acd006567ea475f6e2ea24d83e645b385f
gglsilva <gabrielchamex@gmail.com>
2022-05-24 08:42:09 -0300

    Adciona modelo de dados

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'publicado', 'status')
+    list_filter = ('status', 'publicado', 'criado_em')
+    search_fields = ('titulo', 'conteudo')
+    prepopulated_fields = {'slug': ('titulo',)}
+    raw_id_fields = ('autor',)
+    date_hierarchy = 'publicado'
+    ordering = ('-publicado',)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('rascunho', 'Rascunho'),
+        ('publicado', 'Publicado'),
+    )
+    titulo = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publicado')
+    autor = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    imagem = models.ImageField(upload_to='blog/%Y/%m/%d', blank=True)
+    resumo = models.CharField(max_length=250)
+    conteudo = RichTextUploadingField()
+    publicado = models.DateTimeField(default=timezone.now)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='rascunho')
+
+    class Meta:
+        ordering = ('-publicado',)
+
+    def __str__(self):
+        return self.titulo
+
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publicado.year,
+                        self.publicado.month,
+                        self.publicado.day, 
+                        self.slug])
